2847f7b15b480936ba8d39e8f4f1eb66f77adc7f
amineelhassak <amineatrache57@gmail.com>
2025-08-13 22:12:25 +0100

    modified:   Makefile         modified:   srcs/.env         modified:   srcs/docker-compose.yml         modified:   srcs/requirements/mariadb/Dockerfile         modified:   srcs/requirements/mariadb/tools/script.sh         modified:   srcs/requirements/nginx/conf/config.conf         modified:   srcs/requirements/wordpress/Dockerfile         deleted:    srcs/requirements/wordpress/conf/redis-config.php         deleted:    srcs/requirements/wordpress/tools/inception-products-plugin.php         deleted:    srcs/requirements/wordpress/tools/products.php         deleted:    srcs/requirements/wordpress/tools/setup-products.sh         modified:   srcs/requirements/wordpress/tools/wordpress.sh

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -12,33 +12,19 @@ mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 wp config create --dbname=$DATABASE_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
+
+# Configure Redis cache
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+wp config set WP_REDIS_TIMEOUT 1 --allow-root
+wp config set WP_REDIS_READ_TIMEOUT 1 --allow-root
+wp config set WP_REDIS_DATABASE 0 --allow-root
+
 wp core install --url=$DOMAINE_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_email=$WP_ADMIN_EMAIL --admin_password=$WP_ADMIN_PASSWORD --skip-email --allow-root
 wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 
-# Install and setup products database
-echo "🛍️ Setting up products database..."
-chmod +x /tmp/setup-products.sh
-/tmp/setup-products.sh
-
-# Install Redis Object Cache plugin
+# Install and activate Redis plugin
 wp plugin install redis-cache --activate --allow-root
-wp config set WP_REDIS_HOST "'$REDIS_HOST'" --raw --allow-root
-wp config set WP_REDIS_PORT $REDIS_PORT --raw --allow-root
 wp redis enable --allow-root
 
-# Install our custom products plugin
-echo "📦 Installing Inception Products Plugin..."
-mkdir -p /var/www/html/wp-content/plugins/inception-products
-cp /tmp/inception-products-plugin.php /var/www/html/wp-content/plugins/inception-products/inception-products.php
-wp plugin activate inception-products --allow-root
-
-# Copy test file for debugging
-cp /tmp/test-connection.php /var/www/html/test-db.php
-cp /tmp/products.php /var/www/html/products.php
-
-echo "✅ WordPress-Redis-Products integration completed!"
-echo "🌐 Access your products at: https://amel-has.42.fr/products"
-echo "🌐 Alternative access: https://amel-has.42.fr/products.php"
-echo "🔧 Test database at: https://amel-has.42.fr/test-db.php"
-
-exec $@     
+exec $@
